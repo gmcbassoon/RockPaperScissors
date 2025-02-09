95b865dcd590b825b0c96818386e039cff8081e5
Garrett McCormick <gmcbassoon@gmail.com>
2025-02-09 15:49:52 -0800

    Added all variations of wins

--- javascript.js
+++ javascript.js
@@ -27,11 +27,26 @@ if (humanAnswer === "rock" || "paper" || "scissors") {
 } else {
     alert("Not a valid answer, please try again")
 }
+console.log(humanAnswer);
 // Compare Answers
 function compareAnswer() {
     if (humanAnswer === computerAnswer) {
         alert("It's a tie, nobody wins!");
-    } else if (humanAnswer !== computerAnswer) {
-        
+    } else {
+        if (humanAnswer === "rock" && computerAnswer === "scissors") {
+            alert(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+        } else if (humanAnswer === "scissors" && computerAnswer === "rock") {
+            alert(`Computer Wins! Computer played ${computerAnswer}, Redo?`);
+        } else if (humanAnswer === "paper" && computerAnswer === "rock") {
+            alert(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+        } else if (humanAnswer === "rock" && computerAnswer === "paper") {
+            alert(`Computer Wins! Computer played ${computerAnswer}, Redo?`)
+        } else if (humanAnswer === "scissors" && computerAnswer === "paper") {
+            alert(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+        } else if (humanAnswer === "paper" && computerAnswer === "scissors") {
+            alert(`Computer Wins! Computer played ${computerAnswer}, Redo?`)
+        }
     }
+    return;
 }
+compareAnswer();
