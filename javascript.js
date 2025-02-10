c4015c5368ee6b2abeb9b30751a030f3dc543029
Garrett McCormick <gmcbassoon@gmail.com>
2025-02-10 08:44:01 -0800

    Code now able to be looped

--- javascript.js
+++ javascript.js
@@ -1,10 +1,16 @@
 // Create computerAnswer
+let humanScore = 0;
+let computerScore = 0;
+const numOfRounds = 5;
+for (let i = 1 ; i <= numOfRounds ; i++) {
 function getComputerAnswer(max) {
     return Math.floor(Math.random() * max);
     
     }
-
+let humanScore = 0;
+let computerScore = 0;
 let computerAnswer = (getComputerAnswer(3) + 1);
+
 if (computerAnswer === 1) {
     computerAnswer = "rock";
 } else if(computerAnswer === 2) {
@@ -28,25 +34,45 @@ if (humanAnswer === "rock" || "paper" || "scissors") {
     alert("Not a valid answer, please try again")
 }
 console.log(humanAnswer);
-// Compare Answers
+
 function compareAnswer() {
     if (humanAnswer === computerAnswer) {
-        alert("It's a tie, nobody wins!");
+        prompt("It's a tie, nobody wins!");
+        
     } else {
         if (humanAnswer === "rock" && computerAnswer === "scissors") {
-            alert(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+            prompt(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+            humanScore++;
+            
         } else if (humanAnswer === "scissors" && computerAnswer === "rock") {
-            alert(`Computer Wins! Computer played ${computerAnswer}, Redo?`);
+            prompt(`Computer Wins! Computer played ${computerAnswer}, Redo?`);
+            computerScore++;
+            
         } else if (humanAnswer === "paper" && computerAnswer === "rock") {
-            alert(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+            prompt(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+            humanScore++;
+            
         } else if (humanAnswer === "rock" && computerAnswer === "paper") {
-            alert(`Computer Wins! Computer played ${computerAnswer}, Redo?`)
+            prompt(`Computer Wins! Computer played ${computerAnswer}, Redo?`)
+            computerScore++;
+            
         } else if (humanAnswer === "scissors" && computerAnswer === "paper") {
-            alert(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+            prompt(`Human Wins! Computer played ${computerAnswer}, Redo?`);
+            humanScore++;
+            
         } else if (humanAnswer === "paper" && computerAnswer === "scissors") {
-            alert(`Computer Wins! Computer played ${computerAnswer}, Redo?`)
+            prompt(`Computer Wins! Computer played ${computerAnswer}, Redo?`)
+            computerScore++;
+            
         }
     }
     return;
 }
+}
 compareAnswer();
+console.log(humanScore);
+console.log(computerScore);
+
+function scoreTracker() {
+
+}
