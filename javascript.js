7ab625a61ce4b3270326a177b51240c2b0624cc0
Garrett McCormick <gmcbassoon@gmail.com>
2025-02-23 10:02:45 -0800

    Computer vs Human answer working on button click

--- javascript.js
+++ javascript.js
@@ -2,85 +2,102 @@
 let humanScore = 0;
 let computerScore = 0;
 let tieCounter = 0;
-let numOfRounds = prompt("How many rounds would you like?");
-numOfRounds = parseInt(numOfRounds);
+let numOfRounds = 5;
+const rock = document.querySelector("#rock");
+const paper = document.querySelector("#paper");
+const scissors = document.querySelector("#scissors");
+const paraAnnouncement = document.createElement('p');
+const paraWinner = document.createElement('p');
+console.log(rock)
+console.log(paper)
+console.log(scissors)
 //
-for (let i = 1 ; i <= numOfRounds ; i++) {
+// for (let i = 1 ; i <= numOfRounds ; i++) {
 // Create computerAnswer
+
 function getComputerAnswer(max) {
     return Math.floor(Math.random() * max);
+}
+
+function convertComputerAnswer() {
+    let computerAnswer = (getComputerAnswer(3) + 1);
     
+    if (computerAnswer === 1) {
+        computerAnswer = "rock";
+    } else if(computerAnswer === 2) {
+        computerAnswer = "paper";
+    } else if(computerAnswer === 3) {
+        computerAnswer = "scissors";
+    } else {
+        computerAnswer = "My Shit Broke, please fix";
     }
-
-let computerAnswer = (getComputerAnswer(3) + 1);
-
-if (computerAnswer === 1) {
-    computerAnswer = "rock";
-} else if(computerAnswer === 2) {
-    computerAnswer = "paper";
-} else if(computerAnswer === 3) {
-    computerAnswer = "scissors";
-} else {
-    computerAnswer = "My Shit Broke, please fix";
+    return computerAnswer;
 }
+    
 
-console.log(computerAnswer);
+
+
 // Create humanAnswer
-getHumanAnswer(prompt("I ask you this, shall you defend with Rock, Paper, or Scissors?"))
-function getHumanAnswer(prompt) {
-    humanAnswer = prompt.toLowerCase();
-}
 
-if (humanAnswer === "rock" || "paper" || "scissors") {
 
-} else {
-    alert("Not a valid answer, please try again");
-}
-console.log(humanAnswer);
+// if (humanAnswer === "rock" || "paper" || "scissors") {
+
+// } else {
+//     alert("Not a valid answer, please try again");
+// }
 //Comparison of Answers
-function compareAnswer() {
+function compareAnswer(humanAnswer, computerAnswer) {
+
+    // round++;
     if (humanAnswer === computerAnswer) {
-        alert("It's a tie, nobody wins! Next round?");
+        paraAnnouncement.textContent =("It's a tie, nobody wins! Next round?");
         tieCounter++;
     } else {
         if (humanAnswer === "rock" && computerAnswer === "scissors") {
-            alert(`Human Wins! Computer played ${computerAnswer}, next round?`);
+            paraAnnouncement.textContent = (`Human Wins! Computer played ${computerAnswer}, next round?`);
             humanScore++;
             
         } else if (humanAnswer === "scissors" && computerAnswer === "rock") {
-            alert(`Computer Wins! Computer played ${computerAnswer}, next round?`);
+            paraAnnouncement.textContent =(`Computer Wins! Computer played ${computerAnswer}, next round?`);
             computerScore++;
             
         } else if (humanAnswer === "paper" && computerAnswer === "rock") {
-            alert(`Human Wins! Computer played ${computerAnswer}, next round?`);
+            paraAnnouncement.textContent =(`Human Wins! Computer played ${computerAnswer}, next round?`);
             humanScore++;
             
         } else if (humanAnswer === "rock" && computerAnswer === "paper") {
-            alert(`Computer Wins! Computer played ${computerAnswer}, next round?`);
+            paraAnnouncement.textContent =(`Computer Wins! Computer played ${computerAnswer}, next round?`);
             computerScore++;
             
         } else if (humanAnswer === "scissors" && computerAnswer === "paper") {
-            alert(`Human Wins! Computer played ${computerAnswer}, next round?`);
+            paraAnnouncement.textContent =(`Human Wins! Computer played ${computerAnswer}, next round?`);
             humanScore++;
             
         } else if (humanAnswer === "paper" && computerAnswer === "scissors") {
-            alert(`Computer Wins! Computer played ${computerAnswer}, next round?`);
+            paraAnnouncement.textContent =(`Computer Wins! Computer played ${computerAnswer}, next round?`);
             computerScore++;
             
         }
     }
     
-    return;
+    console.log(computerAnswer)
+    console.log(humanAnswer)
     
 }
+rock.addEventListener('click', () => {compareAnswer("rock", convertComputerAnswer())});
+paper.addEventListener("click", () => {compareAnswer("paper", convertComputerAnswer())});
+scissors.addEventListener("click", () => {compareAnswer("scissors", convertComputerAnswer())});
 
-compareAnswer();
-}
+
+
+// }
 //Declaring the score at the end
 if (humanScore === computerScore) {
-    alert(`It's a tie with ${humanScore} point\(s)! Reload the page to try again.`);
+    paraWinner.textContent =(`It's a tie with ${humanScore} point\(s)! Reload the page to try again.`);
 } else if (humanScore > computerScore) {
-    alert(`Human wins with ${humanScore} point\(s)! Computer had a measly ${computerScore} point\(s). There were ${tieCounter} ties!`);
+    paraWinner.textContent =(`Human wins with ${humanScore} point\(s)! Computer had a measly ${computerScore} point\(s). There were ${tieCounter} ties!`);
 } else {
-    alert(`Computer wins with ${computerScore} point\(s)! Human had a measly ${humanScore} point\(s) There were ${tieCounter} ties!`);
+    paraWinner.textContent =(`Computer wins with ${computerScore} point\(s)! Human had a measly ${humanScore} point\(s) There were ${tieCounter} ties!`);
 }
+
+//Button Interaction
